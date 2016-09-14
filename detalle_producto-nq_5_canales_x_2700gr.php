b184fc0836a46b7c616014b1721ce0235e9e2ac9
jgonzales-gecko <jgonzales@gecko-btl.com>
2016-09-14 17:01:50 -0500

    Cambios v.8.2

--- detalle_producto-nq_5_canales_x_2700gr.php
+++ detalle_producto-nq_5_canales_x_2700gr.php
@@ -1,58 +1,7 @@
+<!-- menu -->
 <?php include('./include/header.php')?>
-    <!-- Start Mobile Menu Area -->
-    <div class="mobile-menu-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 hidden-lg hidden-md">
-                    <div class="mobile-menu">
-                        <nav id="nav-mobile">
-                            <ul>
-                                <li><a href="index.php">Inicio</a>
-                                    <!-- <ul>
-                                        <li><a href="index.php">Home One</a></li>
-                                        <li><a href="index-2.html">Home Two</a></li>
-                                    </ul> -->
-                                </li>
-                                <!-- <li><a href="about-us.html">About us</a></li>
-                                <li><a href="shop.html">Shop</a></li>
-                                <li><a href="blog.html">Blog</a></li>
-                                <li><a href="shop.html">jewllery</a>
-                                    <a href="#">WOMEN CLOTH </a>
-                                    <ul>
-                                        <li><a href="shop.html">casual shirt</a></li>
-                                        <li><a href="shop.html">rikke t-shirt</a></li>
-                                        <li><a href="shop.html">mia top</a></li>
-                                        <li><a href="shop.html">muscle tee</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Pages</a>
-                                    <ul>
-                                        <li><a href="cart.html">Cart</a></li>
-                                        <li><a href="checkout.html">Checkout</a></li>
-                                        <li><a href="blog.html">Blog</a></li>
-                                        <li><a href="blog-details.html">Blog Details</a></li>
-                                        <li><a href="shop.html">Shop Grid</a></li>
-                                        <li><a href="shop-list.html">Shop List</a></li>
-                                        <li><a href="shop-details.html">Shop Details</a></li>
-                                        <li><a href="my-account.html">my account</a></li>
-                                        <li><a href="registration.html">registration</a></li>
-                                        <li><a href="order.html">order</a></li>
-                                        <li><a href="comming-soon.html">Comming soon</a></li>
-                                        <li><a href="wishlist.html">wishlist</a></li>
-                                        <li><a href="login.html">log in</a></li>
-                                        <li><a href="contact.html">Contact</a></li>
-                                        <li><a href="404.html">404</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="contact.html">Contact</a></li> -->
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- End Mobile Menu Area -->
+<!-- menu -->
+<?php include('./include/menu.php')?>
     <!-- Start Shop-Details Header -->
     <div class="heading-banner background">
         <div class="container">
---
+++ sobre_nosotros.php
@@ -0,0 +1,269 @@
+<!-- header -->
+<?php include ('./include/header.php')?>
+<!-- menu -->
+<?php include('./include/menu.php')?>
+<!-- Start About-Us Header -->
+<div class="heading-banner background">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="heading-banner k-padding text-center">
+					<div class="page-banner-title">
+						<h2>Sobre Nosotros</h2>
+					</div>
+				</div>
+				<div class="breadcam">
+					<ul>
+						<li><a href="index.php">Inicio</a></li>
+						<li><i class="fa fa-angle-right"></i><a href="blog.html">SOBRE NOSOTROS</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- End About-Us Header -->
+<!-- Start About-Us Area -->
+<div class="about-us-area k-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-12 col-xs-12">
+				<div class="about-img">
+					<img src="img/about/1.jpg" alt="About Us Image">
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12 col-xs-12">
+				<div class="about-company">
+					<h2>Nuestras cajas mineras</h2>
+					<p>Somos una empresa líder en la industria del plástico, ya que contamos con más de 30 años de experiencia en el mercado, contando con una amplia gama de productos para diversos sectores de del mercado tal como la agroindustria, pesca, minería, avícola, industrial y artículos del hogar.</p>
+					<p>Nos caracterizamos por ser una empresa eficaz, orientada hacia las mejoras continuas que ponemos al alcance de nuestros clientes. Estamos orgullosos de la calidad de servicio de nuestra empresa, porque hemos construido una reputación líder en nuestro rubro. Tenemos cuidado con el Planeta, porque somos conscientes con la problemática del medio ambiente.</p>
+					<blockquote>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation. Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation</p>
+					</blockquote>
+					<!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation. Lorem ipsum dolor sit amet consectetur adipisicing elit sed don eiusmod tempor enim minim veniam quis notrud exercitation</p> -->
+				</div>
+			</div>
+		</div>
+		<div class="row k-padding-t">
+			<div class="col-md-12">
+				<div class="section-heading-two text-center">
+					<h2><span>excelente calidad</span></h2>
+				</div>
+			</div>
+			<div class="our-service">
+				<!-- Start Single Service -->
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="single-service text-center">
+								<span class="service-icon">
+									<img src="img/icon/12.png" alt="">
+								</span>
+						<h4>Usamos los estándares más altos de calidad</h4>
+						<p>Nuestros productos son de excelente calidad, ya que contamos con un excelente equipo profesional al cuidado de cada detalle que lleva todo el proceso de fabricación.</p>
+					</div>
+				</div>
+				<!-- End Single Service -->
+				<!-- Start Single Service -->
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<div class="single-service text-center">
+								<span class="service-icon">
+									<img src="img/icon/13.png" alt="">
+								</span>
+						<h4>Último en Tecnología</h4>
+						<p>Nos encontramos en constante renovación y adquiriendo nuevos conocimientos y tecnologías con el fin de ser competitivos antes las necesidades del mercado.</p>
+					</div>
+				</div>
+				<!-- End Single Service -->
+				<!-- Start Single Service -->
+				<div class="col-md-4 hidden-sm col-xs-12">
+					<div class="single-service text-center">
+								<span class="service-icon">
+									<img src="img/icon/14.png" alt="">
+								</span>
+						<h4>Excelente Atención</h4>
+						<p>Nuestro compromiso con nuestros clientes, es una responsabilidad que tomamos como nuestra.</p>
+					</div>
+				</div>
+				<!-- End Single Service -->
+			</div>
+		</div>
+	</div>
+</div>
+<!-- End About-Us Area-->
+<!-- Start Our Project -->
+<div class="our-project k-padding-b">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="section-heading-two text-center">
+					<h2><span>our Recent work</span></h2>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Start single-project -->
+	<div class="col-md-4 col-sm-4 col-xs-6 no-padding">
+		<div class="single-project">
+			<img src="img/about/1.jpg" alt="">
+			<div class="project-desc">
+				<h2>fashion show 10 woman with</h2>
+			</div>
+		</div>
+	</div>
+	<!-- End single-project -->
+	<!-- Start single-project -->
+	<div class="col-md-4 col-sm-4 col-xs-6 no-padding">
+		<div class="single-project">
+			<img src="img/about/1.jpg" alt="">
+			<div class="project-desc">
+				<h2>fashion show 10 woman with</h2>
+			</div>
+		</div>
+	</div>
+	<!-- End single-project -->
+	<!-- Start single-project -->
+	<div class="col-md-4 col-sm-4 col-xs-6 no-padding">
+		<div class="single-project">
+			<img src="img/about/1.jpg" alt="">
+			<div class="project-desc">
+				<h2>fashion show 10 woman with</h2>
+			</div>
+		</div>
+	</div>
+	<!-- End single-project -->
+	<!-- Start single-project -->
+	<div class="col-md-4 col-sm-4 col-xs-6 no-padding">
+		<div class="single-project">
+			<img src="img/about/1.jpg" alt="">
+			<div class="project-desc">
+				<h2>fashion show 10 woman with</h2>
+			</div>
+		</div>
+	</div>
+	<!-- End single-project -->
+	<!-- Start single-project -->
+	<div class="col-md-4 col-sm-4 col-xs-6 no-padding">
+		<div class="single-project">
+			<img src="img/about/1.jpg" alt="">
+			<div class="project-desc">
+				<h2>fashion show 10 woman with</h2>
+			</div>
+		</div>
+	</div>
+	<!-- End single-project -->
+	<!-- Start single-project -->
+	<div class="col-md-4 col-sm-4 col-xs-6 no-padding">
+		<div class="single-project">
+			<img src="img/about/1.jpg" alt="">
+			<div class="project-desc">
+				<h2>fashion show 10 woman with</h2>
+			</div>
+		</div>
+	</div>
+	<!-- End single-project -->
+</div>
+<!-- End Our Project Area -->
+<!-- Start Our Team Area -->
+<div class="our-team k-padding-b">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="section-heading-two text-center">
+					<h2><span>our Team</span></h2>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<!-- Start single-team-m -->
+			<div class="col-md-4 col-sm-4 col-xs-12">
+				<div class="single-team-m">
+					<div class="member-img">
+						<img src="img/about/2.jpg" alt="">
+						<div class="member-social">
+							<ul>
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fa fa-behance"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="member-desc">
+						<h3>akramul Islam</h3>
+						<span>ceo,  websbucket</span>
+					</div>
+				</div>
+			</div>
+			<!-- End single-team-m -->
+			<!-- Start single-team-m -->
+			<div class="col-md-4 col-sm-4 col-xs-12">
+				<div class="single-team-m">
+					<div class="member-img">
+						<img src="img/about/3.jpg" alt="">
+						<div class="member-social">
+							<ul>
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fa fa-behance"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="member-desc">
+						<h3>akramul Islam</h3>
+						<span>ceo,  websbucket</span>
+					</div>
+				</div>
+			</div>
+			<!-- End single-team-m -->
+			<!-- Start single-team-m -->
+			<div class="col-md-4 col-sm-4 col-xs-12">
+				<div class="single-team-m">
+					<div class="member-img">
+						<img src="img/about/5.jpg" alt="">
+						<div class="member-social">
+							<ul>
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fa fa-behance"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="member-desc">
+						<h3>akramul Islam</h3>
+						<span>ceo,  websbucket</span>
+					</div>
+				</div>
+			</div>
+			<!-- End single-team-m -->
+		</div>
+	</div>
+</div>
+<!-- End Our Team Area -->
+<!-- Start Newsletter Area -->
+<!-- <div class="newsletter-area k-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
+				<div class="newsletter text-center">
+					<h3><span>Subscribe</span>to our newsletter</h3>
+					<p>Subscribe to the Expert mailing list to receive updates on new arrivals, special offers and other discount information.</p>
+					<form action="#">
+						<div class="newsletter-form">
+							<input type="text" placeholder="Your Email.........">
+							<button>subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div> -->
+<!-- End Newsletter Area -->
+<?php include('./include/footer.php')?>
